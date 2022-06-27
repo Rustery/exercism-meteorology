2be4009fc9b3ab2bc33b9e6b7ff2fd5bf8d3a69b
Rustam Khasanshin <rustery@gmail.com>
2022-06-27 15:49:49 +0500

    initial

---
+++ meteorology.go
@@ -0,0 +1,55 @@
+package meteorology
+
+import "fmt"
+
+type TemperatureUnit int
+
+const (
+	Celsius    TemperatureUnit = 0
+	Fahrenheit TemperatureUnit = 1
+)
+
+func (t TemperatureUnit) String() string {
+	return []string{"°C", "°F"}[t]
+}
+
+type Temperature struct {
+	degree int
+	unit   TemperatureUnit
+}
+
+func (t Temperature) String() string {
+	return fmt.Sprintf("%d %s", t.degree, t.unit.String())
+}
+
+type SpeedUnit int
+
+const (
+	KmPerHour    SpeedUnit = 0
+	MilesPerHour SpeedUnit = 1
+)
+
+func (t SpeedUnit) String() string {
+	return []string{"km/h", "mph"}[t]
+}
+
+type Speed struct {
+	magnitude int
+	unit      SpeedUnit
+}
+
+func (t Speed) String() string {
+	return fmt.Sprintf("%d %s", t.magnitude, t.unit.String())
+}
+
+type MeteorologyData struct {
+	location      string
+	temperature   Temperature
+	windDirection string
+	windSpeed     Speed
+	humidity      int
+}
+
+func (t MeteorologyData) String() string {
+	return fmt.Sprintf("%s: %s, Wind %s at %s, %d%% Humidity", t.location, t.temperature.String(), t.windDirection, t.windSpeed.String(), t.humidity)
+}
